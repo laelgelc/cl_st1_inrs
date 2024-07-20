8b7eade70c6fef976d33d79aa397bb079a12db3a
eyamrog <eyamrog@gmail.com>
2024-07-20 19:49:46 -0300

    Add files via upload

--- server_setup.sh
+++ server_setup.sh
@@ -10,26 +10,26 @@ sudo snap install aws-cli --classic
 sudo apt install -y ripgrep html2text unzip python3-pip python3-venv
 
 # Create directory for TreeTagger
-mkdir -p ~/treetagger/
+mkdir -p "$HOME"/treetagger/
 
 # Define base URL for TreeTagger files
 BASE_URL="https://cis.uni-muenchen.de/~schmid/tools/TreeTagger/data"
 
 # Download TreeTagger files
-curl -O ${BASE_URL}/tree-tagger-linux-3.2.5.tar.gz -o ~/treetagger/$(basename ${BASE_URL}/tree-tagger-linux-3.2.5.tar.gz)
-curl -O ${BASE_URL}/tagger-scripts.tar.gz -o ~/treetagger/$(basename ${BASE_URL}/tagger-scripts.tar.gz)
-curl -O ${BASE_URL}/install-tagger.sh -o ~/treetagger/$(basename ${BASE_URL}/install-tagger.sh)
-curl -O ${BASE_URL}/english.par.gz -o ~/treetagger/$(basename ${BASE_URL}/english.par.gz)
-curl -O ${BASE_URL}/portuguese2.par.gz -o ~/treetagger/$(basename ${BASE_URL}/portuguese2.par.gz)
+curl -O ${BASE_URL}/tree-tagger-linux-3.2.5.tar.gz -o "$HOME"/treetagger/$(basename ${BASE_URL}/tree-tagger-linux-3.2.5.tar.gz)
+curl -O ${BASE_URL}/tagger-scripts.tar.gz -o "$HOME"/treetagger/$(basename ${BASE_URL}/tagger-scripts.tar.gz)
+curl -O ${BASE_URL}/install-tagger.sh -o "$HOME"/treetagger/$(basename ${BASE_URL}/install-tagger.sh)
+curl -O ${BASE_URL}/english.par.gz -o "$HOME"/treetagger/$(basename ${BASE_URL}/english.par.gz)
+curl -O ${BASE_URL}/portuguese2.par.gz -o "$HOME"/treetagger/$(basename ${BASE_URL}/portuguese2.par.gz)
 
 # Install TreeTagger
-~/treetagger/install-tagger.sh
+"$HOME"/treetagger/install-tagger.sh
 
 # Append TreeTagger paths to .bashrc
-echo >> ~/.bashrc
-echo "# The following lines add TreeTagger to the PATH variable" >> ~/.bashrc
-echo "export PATH=\$PATH:/home/ubuntu/treetagger/cmd" >> ~/.bashrc
-echo "export PATH=\$PATH:/home/ubuntu/treetagger/bin" >> ~/.bashrc
+echo >> "$HOME"/.bashrc
+echo "# The following lines add TreeTagger to the PATH variable" >> "$HOME"/.bashrc
+echo "export PATH=\$PATH:/home/ubuntu/treetagger/cmd" >> "$HOME"/.bashrc
+echo "export PATH=\$PATH:/home/ubuntu/treetagger/bin" >> "$HOME"/.bashrc
 
 # Set up Python virtual environment
 python3 -m venv my_env
